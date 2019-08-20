d89d85b2d699c1e35a3626df96c0b735612031dc
orikarudo <okaccounts@gmail.com>
2019-08-19 22:58:01 -0400

    set up buttons and other widgets

---
+++ Storing data app/Storing data app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Storing data app
+//
+//  Created by Ori Karudo on 8/19/19.
+//  Copyright © 2019 Ori Karudo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
